a2f5b513ccda6e9238d3c7214897a5a165dab38a
mutiaistiqomah23 <mutiaistiqomah@upi.edu>
2023-03-01 13:08:40 +0700

    cireng

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger-menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+}
+
+// Klik di luar side bar untuk menghilangkan Nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+});
+
+
+
